b04a721da86a95c5e3af688739aa1e00c950e21a
Batel Shkolnik <batelshk@post.bgu.ac.il>
2021-04-17 16:50:41 +0300

    Added explanation for the methods.

--- src/PolyMath/Scalar.java
+++ src/PolyMath/Scalar.java
@@ -2,13 +2,13 @@ package PolyMath;
 
 public interface Scalar {
 
-    public Scalar add(Scalar s);
-    public Scalar mul(Scalar s);
+    public Scalar add(Scalar s);//accepts a scalar argument and returns a scalar which is the sum of the current scalar and the argument.
+    public Scalar mul(Scalar s); //accepts a scalar argument and returns a scalar which is the multiplication of the current scalar and the argument.
     public Scalar addRational(Rational s);
     public Scalar addInteger(Integer s);
     public Scalar mulRational(Rational s);
     public Scalar mulInteger(Integer s);
-    public Scalar power(int exponent);
-    public int sign();
-    public Scalar neg();
+    public Scalar power(int exponent); //accepts a scalar argument and returns a new scalar which is the scalar raised to the power of the exponent argument.
+    public int sign(); //returns 1 for positive scalar, -1 for negative and 0 for 0.
+    public Scalar neg(); //returns the scalar multiplied by (-1).
 }
